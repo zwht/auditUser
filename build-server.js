872d6895aafac6e11b19dcf7e6271a59d75b8845
zhaowei <1512763623@qq.com>
2018-01-31 23:10:37 +0800

    8080

--- build-server.js
+++ build-server.js
@@ -8,14 +8,14 @@ var express = require('express');
 var proxyMiddleWare = require("http-proxy-middleware");
 
 var proxyPath = "http://localhost/ums/index.php";//目标后端服务地址(公司同事电脑地址)
-var proxyPath = "http://api.96789.ltd:8080/index.php";
+var proxyPath = "http://120.79.218.81/ums/index.php";
 var proxyPath1 = "http://idmanager-server-ip:9000";//老版本地址
 var app = express();
 
 var proxyOption ={target:proxyPath,changeOrigoin:true};
 var proxyOption1 ={target:proxyPath1,changeOrigoin:true};
 
-app.set('port', (process.env.PORT || 8999));
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use("/api", proxyMiddleWare(proxyOption));
 app.use("/rest", proxyMiddleWare(proxyOption1));

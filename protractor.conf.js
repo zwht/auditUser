e67a0518d07d2b21b6688b70918f5543633fffad
zhaowei <1512763623@qq.com>
2018-02-01 18:15:17 +0800

    shd

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:9001/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
